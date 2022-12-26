6994be967a310d728574e1302a3908ce9810dc4e
Ethan Rybka <moddsplaysmc@gmail.com>
2022-12-25 22:31:00 -0600

    fixed format

--- Code/FarmingBot/FarmingBot.ino
+++ Code/FarmingBot/FarmingBot.ino
@@ -5,10 +5,10 @@
 //Farmbot code V2.0
 //ALL PINS ARE ON PCB DESIGN
 
-const int pump = 3;  //pump relay pin 
-const int sensor1 = A0; // Soil Sensor input at Analog PIN A0 and A1
-const int sensor2 = A1;
-const int lights = 4;   //light relay pin
+#define pump 3;  //pump relay pin 
+#define sensor1 A0; // Soil Sensor input at Analog PIN A0 and A1
+#define sensor2 A1;
+#define lights 4;   //light relay pin
 
 const int threshold = 500; //threshold to change when pump is on or off
 
@@ -17,13 +17,13 @@ RTC_DS1307 rtc; //init RTC obj
 LiquidCrystal_I2C lcd(0x3F, 16, 2); //init LCD obj
 
 void setup() {
-   lcd.init();
-   lcd.backlight();
-   pinMode(pump, OUTPUT);
-   pinMode(lights, OUTPUT);
-   Serial.begin(57600);
-   digitalWrite(pump, HIGH);
-   digitalWrite(lights, HIGH);
+  lcd.init();
+  lcd.backlight();
+  pinMode(pump, OUTPUT);
+  pinMode(lights, OUTPUT);
+  Serial.begin(57600);
+  digitalWrite(pump, HIGH);
+  digitalWrite(lights, HIGH);
 
 #ifndef ESP8266
   while (!Serial); // wait for serial port to connect. Needed for native USB
@@ -41,23 +41,23 @@ if (! rtc.begin()) {
 
 
 boolean isntWet(){
-   int sens1 = analogRead(sensor1);
-   int sens2 = analogRead(sensor2);
-   int avg = (sens1+sens2)/2;
-   lcd.print("Sensor 1: " + sens1);
-   lcd.setCursor(0, 1);              //moves cursor to next line
-   lcd.print("Sensor 2: " + sens2);
-   lcd.setCursor(0, 0);              //moves cursor back up TO FIRST LINE
-   
-   return !(avg > threshold); //if avg is greater than threshold, return true
+  int sens1 = analogRead(sensor1);
+  int sens2 = analogRead(sensor2);
+  int avg = (sens1+sens2)/2;
+  lcd.print("Sensor 1: " + sens1);
+  lcd.setCursor(0, 1);              //moves cursor to next line
+  lcd.print("Sensor 2: " + sens2);
+  lcd.setCursor(0, 0);              //moves cursor back up TO FIRST LINE
+  
+  return !(avg > threshold); //if avg is greater than threshold, return true
 }
 
 boolean isNight(){
-   int hour = rtc.now().hour();
-   if (hour > 19 || hour < 6){ //turns lights on from 7PM to 6AM
-    return true;
-   }
-   return false;
+  int hour = rtc.now().hour();
+  if (hour > 19 || hour < 6){ //turns lights on from 7PM to 6AM
+   return true;
+  }
+  return false;
 }
 
 
