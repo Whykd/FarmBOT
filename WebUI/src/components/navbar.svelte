96a2a4efee4fcbf794db713c3252ae0bfa5458c3
TwitchC4T <74559314+TwitchC4T@users.noreply.github.com>
2023-02-24 08:27:46 -0600

    a

---
+++ WebUI/src/components/navbar.svelte
@@ -0,0 +1,32 @@
+<body>
+    <h1>FarmBot</h1>
+    <div class="container"> 
+        <a href="/liveview">Live View</a>
+        <a href="/manpan">Management Panel</a>
+        <a href="/graph">Data Graphs</a> 
+    </div>
+    
+</body>
+<style>
+    body {
+        
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+    }
+    h1 {
+        font-size: 50px;
+    }
+    .container {
+        display: grid;
+        grid-template-columns: .1fr .3fr .1fr;
+        grid-template-rows: auto;
+        grid-gap: 10px;
+        justify-content: space-evenly;        
+    }
+    a {
+        font-size: 30px;
+        text-decoration: none;
+        color: black;
+
+    }
+</style>
---
+++ WebUI/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    export let data: LayoutData;
+    import Navbar from '../components/navbar.svelte';
+    
+</script>
+<Navbar />
+<slot />
