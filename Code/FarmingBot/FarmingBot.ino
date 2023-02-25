3e0ebb437699489c0648b6d24149091ddc573c1d
Whykd <110788218+Whykd@users.noreply.github.com>
2023-02-25 11:14:08 -0600

    Update FarmingBot.ino

--- Code/FarmingBot/FarmingBot.ino
+++ Code/FarmingBot/FarmingBot.ino
@@ -1,28 +1,27 @@
-#include <RTClib.h>//Adafruit lib
+#include <RTClib.h>            //Adafruit lib
 #include <LiquidCrystal_I2C.h> //By Marco Shwartz
 #include <Wire.h>
 
-//Farmbot code V2.0
-//ALL PINS ARE ON PCB DESIGN
+// Farmbot code V2.0
+// ALL PINS ARE ON PCB DESIGN
 
-#define pump 4  //pump relay pin 
-#define lights 3   //light relay pin
+#define pump 4   // pump relay pin
+#define lights 3 // light relay pin
 
-//define timing array
-//ights on outlet 2
-//pump outlet 3
+// define timing array
+// lights on outlet 2
+// pump outlet 3
 
-const int threshold = 490; //threshold to change when pump is on or off
+#define threshold 490 // threshold to change when pump is on or off
 
+RTC_DS1307 rtc;                     // init RTC obj
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // init LCD obj
 
-RTC_DS1307 rtc; //init RTC obj
-LiquidCrystal_I2C lcd(0x3F, 16, 2); //init LCD obj
+void setup()
+{
+  // pumpon hour, pumpon minute, pumpoff minute, lighton hour, lightoff hour
 
-void setup() {
-  //pumpon hour, pumpon minute, pumpoff minute, lighton hour, lightoff hour
-  
-  int[] timings = {8,45,47,7,19}
-  lcd.init();
+  int[] timings = {8, 45, 47, 7, 19} lcd.init();
   lcd.backlight();
   pinMode(pump, OUTPUT);
   pinMode(lights, OUTPUT);
@@ -30,65 +29,68 @@ void setup() {
   digitalWrite(pump, HIGH);
   digitalWrite(lights, HIGH);
   rtc.begin();
-//if (! rtc.begin()) {
-// Serial.println("Couldn't find RTC");
-//  while (1) delay(10);
-//}.
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //sets rtc to time when code is compiled
+  // if (! rtc.begin()) {
+  //  Serial.println("Couldn't find RTC");
+  //   while (1) delay(10);
+  // }.
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // sets rtc to time when code is compiled
 }
 
-
-
-boolean isntWet(){
+boolean isntWet()
+{
   int sens1 = analogRead(A0);
   Serial.println(sens1);
   int sens2 = analogRead(A1);
   Serial.println(sens2);
-  float avg = (sens1+sens2)/2;
+  float avg = (sens1 + sens2) / 2;
   Serial.println(avg);
-  lcd.print("AVG: "); 
-  lcd.print(avg);            
-         
-  
-  return (avg > threshold); //if avg is greater than threshold, return true
+  lcd.print("AVG: ");
+  lcd.print(avg);
+
+  return (avg > threshold); // if avg is greater than threshold, return true
 }
 
-boolean isNight(){
+boolean isNight()
+{
   int hour = rtc.now().hour();
-  if (hour >= timings[3] && hour <= timings[4]){ //turns lights on from 7AM to 7PM
-   Serial.println("day");
-   return true;
-   
+  if (hour >= timings[3] && hour <= timings[4])
+  { // turns lights on from 7AM to 7PM
+    Serial.println("day");
+    return true;
   }
   return false;
 }
 
-
-void loop() {
+void loop()
+{
   bool night = isNight();
-  if (night){
-    digitalWrite(lights, LOW); //turns lights on
+  if (night)
+  {
+    digitalWrite(lights, LOW); // turns lights on
   }
-  else{
-    digitalWrite(lights, HIGH); //turns lights off
+  else
+  {
+    digitalWrite(lights, HIGH); // turns lights off
   }
   DateTime time = rtc.now();
-  if(time.hour() == timings[0] && time.minute() >= timings[1] && time.minute() <= timings[3]){
-    digitalWrite(pump, LOW); 
+  if (time.hour() == timings[0] && time.minute() >= timings[1] && time.minute() <= timings[2])
+  {
+    digitalWrite(pump, LOW);
   }
-  else {
+  else
+  {
     digitalWrite(pump, HIGH);
   }
   int sens1 = analogRead(A0);
   int sens2 = analogRead(A1);
-  float avg = (sens1+sens2)/2;
-  lcd.setCursor(0,0);
-  lcd.print("AVG: "); 
-  lcd.print(avg); 
+  float avg = (sens1 + sens2) / 2;
+  lcd.setCursor(0, 0);
+  lcd.print("AVG: ");
+  lcd.print(avg);
   lcd.setCursor(0, 1);
   lcd.print(rtc.now().hour());
   lcd.print(":");
   lcd.print(rtc.now().minute());
-  delay(1000); 
+  delay(1000);
   lcd.clear();
 }
