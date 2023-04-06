fdb01f1b081df2b005240073366799fe07e3f42c
Whykd <110788218+Whykd@users.noreply.github.com>
2023-04-06 11:52:44 -0500

    Update +page.svelte

--- WebUI/src/routes/liveview/+page.svelte
+++ WebUI/src/routes/liveview/+page.svelte
@@ -1,1 +1,8 @@
-<embed type="text/html" src="10.20.180.61:8081/?action=stream" width="640" height="480 ">
+<head>
+    <meta http-Equiv="Cache-Control" Content="no-cache" />
+    <meta http-Equiv="Pragma" Content="no-cache" />
+    <meta http-Equiv="Expires" Content="0" />
+</head>
+<body>
+<iframe src="http://10.20.180.61:8081/?action=stream" width="640" height="480"></iframe>
+</body>
