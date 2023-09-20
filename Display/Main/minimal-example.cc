acd38fb1ba8fdfb1df5ea7d1a6cabdac2fe21500
Whykd <110788218+Whykd@users.noreply.github.com>
2023-09-20 11:45:17 -0700

    update file to match pi

--- Display/Main/minimal-example.cc
+++ Display/Main/minimal-example.cc
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <math.h>
 #include <stdio.h>
-#include <signal.h>
+#include <signal.h> 
 
 #include <fstream>
 #include <nlohmann/json.hpp>
@@ -35,44 +35,49 @@ static void DrawOnCanvas(Canvas *canvas) {
    * Let's create a simple animation. We use the canvas to draw
    * pixels. We wait between each step to have a slower animation.
    */
-    canvas->Fill(100, 100, 100);
+    canvas->Fill(0, 0, 0);
     usleep(1 * 10000);  // wait a little to slow down things.
-    for (int y = 0; y < 32; y++){
-      for (int x = 0; x < 64; x++){
-        if (interrupt_received)
-          return;
-        int red = (rand()%256);
-        int g = (rand()%256);
-        int b = (rand()%256);
-        canvas->SetPixel(x, y, red, g, b); 
-      }
+    for (int rows = 0; rows < 32; rows++){
+      	canvas->SetPixel(0, rows, 0, 0, 0);
+      	canvas->SetPixel(1, rows, 0, 0, 0);
+      	canvas->SetPixel(62, rows, 0, 0, 0);
+      	canvas->SetPixel(63, rows, 0, 0, 0);
     }
-    usleep(1 * 10000); 
+    for (int vals = 0; vals < 10; vals++){
+        if(interrupt_received) return;
+		int value = data[to_string(vals)]["Sensors"]["avg"];
+		value = ((value * 32)/1024) + 1;
+		for (int row = 1; row <= value; row++){
+			canvas->SetPixel(vals + 2, 31 - (row-1), 100, 100, 100);
+			if(interrupt_received) return;
+		}
+    }
+    usleep(1 * 10000); //delay to be removed later maybe
 }
 
 int main(int argc, char *argv[]) {
-  cout << data;
-  RGBMatrix::Options defaults;
-  defaults.hardware_mapping = "adafruit-hat";  // or e.g. "adafruit-hat"
-  defaults.rows = 32;
-  defaults.chain_length = 1;
-  defaults.parallel = 1;
-  defaults.show_refresh_rate = false;
-  Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
-  if (canvas == NULL)
-    return 1;
+  	RGBMatrix::Options defaults;
+  	defaults.hardware_mapping = "adafruit-hat";  // or e.g. "adafruit-hat"
+  	defaults.rows = 32;
+  	defaults.cols = 64;
+  	defaults.chain_length = 1;
+  	defaults.parallel = 1;
+  	defaults.show_refresh_rate = false;
+  	Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+  	if (canvas == NULL)
+    	return 1;
 
   // It is always good to set up a signal handler to cleanly exit when we
   // receive a CTRL-C for instance. The DrawOnCanvas() routine is looking
   // for that.
-  signal(SIGTERM, InterruptHandler);
-  signal(SIGINT, InterruptHandler);
+  	signal(SIGTERM, InterruptHandler);
+  	signal(SIGINT, InterruptHandler);
 
-  DrawOnCanvas(canvas);    // Using the canvas.
+  	DrawOnCanvas(canvas);    // Using the canvas.
 
   // Animation finished. Shut down the RGB matrix.
-  canvas->Clear();
-  delete canvas;
+  	canvas->Clear();
+  	delete canvas;
 
-  return 0;
+  	return 0;
 }
