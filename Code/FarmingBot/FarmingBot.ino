8ba377881a85ac4fe398119f6679d3e7845f2ab2
Whykd <110788218+Whykd@users.noreply.github.com>
2023-11-14 16:59:28 -0600

    v2 start

--- Code/FarmingBot/FarmingBot.ino
+++ Code/FarmingBot/FarmingBot.ino
@@ -1,71 +1,74 @@
 #include <RTClib.h>            //Adafruit lib
-#include <LiquidCrystal_I2C.h> //By Frank de Brabander
-#include <Wire.h>
+#include <ArduinoJson.h>       //by bblanchon
 
-// Farmbot code V1.0.0
-// ALL PINS ARE ON PCB DESIGN
+bool pumpEnabled = true;
+bool lightEnable = true;
 
-// lights on outlet 2
-// pump outlet 3
-#define pump 4        // pump relay pin
-#define lights 3      // light relay pin
+int bypassPumpState = false;
+int bypassLightState = false;
 
-RTC_DS1307 rtc;                     // init RTC obj
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // init LCD obj
-//pump on/off hour, pump on min, pump off min, light on hour, light off hour 
-uint8_t timings[] = {9, 35, 37, 7, 19};  // define timing array
+int pHour = 0;
+int pMinuteOn = 0;
+int pMinuteOff = 0;
+int lHourOn = 0;
+int lHourOff = 0;
 
-void setup()
-{
-  lcd.init();
-  lcd.backlight();
-  pinMode(pump, OUTPUT);
-  pinMode(lights, OUTPUT);
-  Serial.begin(57600);
-  digitalWrite(pump, HIGH);
-  digitalWrite(lights, HIGH);
-  rtc.begin();
-  //`rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // sets rtc to time when code is compiled
+void setup(){
+
 }
 
-boolean isNight()
-{
-  int hour = rtc.now().hour();
-  if (hour >= timings[3] && hour <= timings[4])
-  { // turns lights on from start hour to end hour
-    return true;
-  }
-  return false;
-}
+void loop(){
+DynamicJsonDocument doc(1024); //create a json document
+deserilaizeJson(doc, Serial); //deserialize the json document from the serial port
+/*
+ *
+ *  0 - do nothing
+ *  1 - toggle pump function
+ *  2 - toggle light function
+ *  3 - update pump and light schedule from json
+ *  4 - toggle light on/off
+ *  5 - toggle pump on/off
+ *  6 - update rtc time/date
+ * 
+ */
 
 
-void loop()
-{
-  bool night = isNight();
-  if (night){
-    digitalWrite(lights, LOW); // turns lights on
+int action = doc["action"]; //get the action from the json document
+  switch (action)
+  {
+  case 0:
+    break;
+
+  case 1:
+    pumpEnabled = !pumpEnabled;
+    break;
+
+  case 2:
+    lightEnable = !lightEnable;
+    break;
+
+  case 3:
+    pHour = doc["data"]["pHour"];
+    pMinuteOn = doc["data"]["pMinuteOn"];
+    pMinuteOff = doc["data"]["pMinuteOff"];
+
+    lHourOn = doc["data"]["lHourOn"];
+    lHourOff = doc["data"]["lHourOff"];
+    break;
+  case 4:
+    bypassPumpState = !bypassPumpState;
+    break;
+
+  case 5:
+    bypassLightState = !bypassLightState;
+    break;
+
+  case 6:
+    //rtc to deal with later
+    break;
+
+  default:
+    break;
   }
-  else{
-    digitalWrite(lights, HIGH); // turns lights off
-  }
-  DateTime time = rtc.now();
-  //runs pump for 2 minutes every other day.
-  if (time.hour() == timings[0] && time.minute() >= timings[1] && time.minute() <= timings[2] && time.day()%2 == 0){
-    digitalWrite(pump, LOW);
-  }
-  else{
-    digitalWrite(pump, HIGH);
-  }
-  lcd.setCursor(0, 1);
-  lcd.print(rtc.now().hour());
-  lcd.print(":");
-  if(rtc.now().minute()<10){
-    lcd.print("0");
-    lcd.print(rtc.now().minute());
-  }
-  else{
-    lcd.print(rtc.now().minute());
-  }
-  delay(1000);
-  lcd.clear();
+
 }
